9385b8ddb570b1e961a44097b16210c8522ac65d
Gah2k0 <gah.francisco@gmail.com>
2022-04-14 16:35:33 -0300

    Delete adicionado

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -146,6 +146,34 @@ function updateTask(id) {
      )
 }
 
+let configuracaoDeleteAutorizado = {
+
+    headers: {
+
+        'Authorization': localStorage.getItem('token')
+        
+    },
+    method: 'DELETE',
+}
+
+function deleteTask(id) {
+
+    
+    fetch(`https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`, configuracaoDeleteAutorizado).then(
+
+        response => {
+
+            if(response.ok) {
+
+                getTasks()
+                //console.log(response)
+            }
+
+        }
+     )
+
+}
+
 function renderTasks(tasks) {
 
     tarefasPendentesReference.innerHTML = ''
@@ -162,12 +190,13 @@ function renderTasks(tasks) {
         tarefasPendentesReference.innerHTML += `    
                     <div>
                         <li class="tarefa">
-                        <div class="not-done" onclick="updateTask(${task.id})"></div>
-                        <div class="descricao">
-                            <p class="nome">${task.description}</p>
-                            <p class="timestamp">${dataFormatada}</p>
+                            <div class="not-done" onclick="updateTask(${task.id})"></div>
+                            <div class="descricao">
+                                <p class="nome">${task.description}</p>
+                                <p class="timestamp">${dataFormatada}</p>
                             </div>
-                        </li>
+                                <img onclick="deleteTask(${task.id})" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAABfUlEQVRoge2YoU4DQRCG/7mro+Wuvjg8JCXhHRC8BAmmtRCqQEIguIoqPMEhEPAKfQECDgvljuLIDYYmFO56O+zS5ch8ZsU/sztf9tYcoCiK4hNyuVl/d3gAxv6sGmY+6p6s7bk605mAyfATXEoYCVye3W8gywYAWi4ONeCBQdubW8tXZYWB0XbzHR4AWgQemBSaCcx3+AlLJkWmAn8W0SPu7wz5twb5TOe4bTxX5W9ABXxTeYGaTfNo9II0fUW0uIC42fiWJckYABBH9dy8qFeC1Q0kyRhZluH5Y9CvGTODmQvzol4JVgLMPLXmZWV5Xiah8m9ABXyjAr5RAd+ogG9UwDcq4BsV8I0K+MZKgIim1rysLM/LJFgJxFEdQUCIonpuRkQgKs6LeiVY/ZWIm43CPwqzMpPcFOkNpNYnlpNIimUCjBtR/U9gvpaUiwQ4C3sAHkUDSfYHPaFGPUmPSKB7unobvIUrIJzD7eeUgvmCQl7vHLbvHO6rKP+ed2yBftABMd1OAAAAAElFTkSuQmCC" />
+                                </li>
                     </div>`
         } else {
 
@@ -179,11 +208,12 @@ function renderTasks(tasks) {
             tarefasFinalizadasReference.innerHTML += `    
             <div>
             <li class="tarefa">
-            <div class="done" onclick="updateTask(${task.id})"></div>
-            <div class="descricao">
-                <p class="nome">${task.description}</p>
-                <p class="timestamp">${dataFormatada}</p>
-            </div>
+                <div class="done" onclick="updateTask(${task.id})"></div>
+                <div class="descricao">
+                    <p class="nome">${task.description}</p>
+                    <p class="timestamp">${dataFormatada}</p>
+                </div>
+                <img onclick="deleteTask(${task.id})" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAABfUlEQVRoge2YoU4DQRCG/7mro+Wuvjg8JCXhHRC8BAmmtRCqQEIguIoqPMEhEPAKfQECDgvljuLIDYYmFO56O+zS5ch8ZsU/sztf9tYcoCiK4hNyuVl/d3gAxv6sGmY+6p6s7bk605mAyfATXEoYCVye3W8gywYAWi4ONeCBQdubW8tXZYWB0XbzHR4AWgQemBSaCcx3+AlLJkWmAn8W0SPu7wz5twb5TOe4bTxX5W9ABXxTeYGaTfNo9II0fUW0uIC42fiWJckYABBH9dy8qFeC1Q0kyRhZluH5Y9CvGTODmQvzol4JVgLMPLXmZWV5Xiah8m9ABXyjAr5RAd+ogG9UwDcq4BsV8I0K+MZKgIim1rysLM/LJFgJxFEdQUCIonpuRkQgKs6LeiVY/ZWIm43CPwqzMpPcFOkNpNYnlpNIimUCjBtR/U9gvpaUiwQ4C3sAHkUDSfYHPaFGPUmPSKB7unobvIUrIJzD7eeUgvmCQl7vHLbvHO6rKP+ed2yBftABMd1OAAAAAElFTkSuQmCC" />
             </li>
         </div>`
         }
